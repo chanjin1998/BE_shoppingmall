b2fcb61c8a04c9343ff6c969f710f3afb18dbd3d
chanjin1998 <chanjin@kookmin.ac.kr>
2024-05-23 10:37:00 +0900

    fix: integer 타입에 @NotBlank 사용 수정

--- src/main/java/com/example/shoppingmall/Product/ProductDTO.java
+++ src/main/java/com/example/shoppingmall/Product/ProductDTO.java
@@ -1,6 +1,7 @@
 package com.example.shoppingmall.Product;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,14 +14,14 @@ public class ProductDTO {
     @NotBlank(message = "상품명은 필수 입력입니다.")
     private String name;
 
-    @NotBlank(message = "상품가격은 필수 입력입니다.")
+    @NotNull(message = "상품가격은 필수 입력입니다.")
     private int price;
 
     @NotBlank(message = "상품설명은 필수 입력입니다.")
     @Size(min = 1, max = 100, message = "상품 설명은 1~100자 이내입니다.")
     private String description;
 
-    @NotBlank(message = "카테고리명은 필수 입력입니다.")
+    @NotNull(message = "카테고리명은 필수 입력입니다.")
     private int categoryId;
 
     public Product converToEntity(){
