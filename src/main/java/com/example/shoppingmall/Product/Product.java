aecda7aed9731943664b48c95d9002ba1f110a96
chanjin1998 <chanjin@kookmin.ac.kr>
2024-05-23 09:40:12 +0900

    feat: @Entity 추가, 생성자 추가

--- src/main/java/com/example/shoppingmall/Product/Product.java
+++ src/main/java/com/example/shoppingmall/Product/Product.java
@@ -1,17 +1,31 @@
 package com.example.shoppingmall.Product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private int price;
     private String description;
-    private int id;
     private int categoryId;
 
     // setter는 가능한 도메인 객체에는 만들지 말자!
-
+    public Product(String name, int price, String description, int categoryId){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
 }
